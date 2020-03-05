442d34b08235c4edbcb8b4f0be6ec48684c0a9c8
Elliot Redhead <elliot.redhead@gmail.com>
2020-03-05 22:16:41 +0000

    Created profile page, imported requirement.

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect, reverse
 from django.contrib import auth, messages
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
 from accounts.forms import UserLoginForm, UserRegistrationForm
 
 
@@ -47,7 +48,8 @@ def registration(request):
         if registration_form.is_valid():
             registration_form.save()
             user = auth.authenticate(
-                username=request.POST["username"], password=request.POST["password1"])
+                username=request.POST["username"],
+                password=request.POST["password1"])
             if user:
                 auth.login(user=user, request=request)
                 messages.success(request, "You have successfully registered.")
